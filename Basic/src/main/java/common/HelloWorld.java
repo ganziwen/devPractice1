6eecd9bd28fa72b3758ecac4632d1a731e9175c4
steven01.gan <steven01.gan@vipshop.com>
2021-07-01 22:47:01 +0800

    first_one

---
+++ Basic/src/main/java/common/HelloWorld.java
@@ -0,0 +1,12 @@
+package common;
+
+/**
+ * @author steven01.gan
+ * @version 1.0
+ * @date 2021/7/1-22:41
+ */
+public class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("hello world");
+    }
+}
---
+++ src/main/java/common/Hello.java
@@ -0,0 +1,12 @@
+package common;
+
+/**
+ * @author steven01.gan
+ * @version 1.0
+ * @date 2021/7/1-22:42
+ */
+public class Hello {
+    public static void main(String[] args) {
+        System.out.println("hello");
+    }
+}
