8ab5231e58c988d0eb1c83c72c787953207466dd
steven01.gan <steven01.gan@vipshop.com>
2021-07-13 20:38:30 +0800

    ✨ jdbc 完善几个异常

--- Basic/src/main/java/jdbc/Demo1.java
+++ Basic/src/main/java/jdbc/Demo1.java
@@ -3,7 +3,7 @@ package jdbc;
 import java.sql.*;
 
 /**
- * jdbc 第一个示例
+ * jdbc 第一个示例,基本操作流程
  *
  * @author steven01.gan
  * @version 1.0
@@ -14,21 +14,27 @@ public class Demo1 {
         String url = "jdbc:mysql://www.xiaowenshu.cn:3388/test_table?useUnicode=true&characterEncoding=utf-8&useSSL=false&serverTimezone=UTC";
         String userName = "root";
         String password = "123456";
-
-        // 建立连接池
-        Connection connection = DriverManager.getConnection(url, userName, password);
         String sql = "SELECT * from tb_user";
 
-        // 建立声明(类似 Py 的游标)
-        Statement statement = connection.createStatement();
+        // 1. 建立连接
+        Connection connection = DriverManager.getConnection(url, userName, password);
 
-        // 执行查询
+        // 2. 具体操作
+        // 2.1 预编译 sql
+        PreparedStatement statement = connection.prepareStatement(sql);
+
+        // 2.2 执行查询
         ResultSet resultSet = statement.executeQuery(sql);
+
+        // 2.3 处理结果集
         while (resultSet.next()) {
+            // 按照列名去查
             System.out.println(resultSet.getLong("id"));
+            // 按照列索引去查,从 1 开始(没啥用,还是列名来的直观)
+            System.out.println(resultSet.getString(3));
             System.out.println(resultSet.getString("user_name"));
         }
-        // 一级一级关闭;其实直接关闭 connection 即可,其他俩直接会被关闭
+        // 3. 资源关闭：一级一级关闭;其实直接关闭 connection 即可,其他俩直接会被关闭
         resultSet.close();
         statement.close();
         connection.close();
---
+++ Basic/src/main/java/jdbc/Demo2.java
@@ -0,0 +1,73 @@
+package jdbc;
+
+import java.sql.*;
+
+/**
+ * 相对规范的写法(补充异常信息)
+ *
+ * @author steven01.gan
+ * @version 1.0
+ * @date 2021/7/13-15:26
+ */
+public class Demo2 {
+    public static void main(String[] args) {
+        String url = "jdbc:mysql://www.xiaowenshu.cn:3388/test_table?useUnicode=true&characterEncoding=utf-8&useSSL=false&serverTimezone=UTC";
+        String userName = "root";
+        String password = "123456";
+        String sql = "SELECT * from tb_user";
+
+        Connection connection = null;
+        PreparedStatement statement = null;
+        ResultSet resultSet = null;
+        try {
+            // 1. 建立连接
+            connection = DriverManager.getConnection(url, userName, password);
+
+            // 2. 具体操作
+            // 2.1 预编译 sql
+            statement = connection.prepareStatement(sql);
+
+            // 2.2 执行查询
+            resultSet = statement.executeQuery(sql);
+
+            // 2.3 处理结果集
+            while (resultSet.next()) {
+                // 按照列名去查
+                System.out.println(resultSet.getLong("id"));
+
+                // 按照列索引去查,从 1 开始(没啥用,还是列名来的直观)
+                // System.out.println(resultSet.getString(3));
+                System.out.println(resultSet.getString("user_name"));
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        } finally {
+
+            // 3. 资源关闭：一级一级关闭;其实直接关闭 connection 即可,其他俩直接会被关闭
+            if (resultSet != null) {
+                try {
+                    resultSet.close();
+                } catch (SQLException e) {
+                    e.printStackTrace();
+                }
+            }
+            if (statement != null) {
+                try {
+                    resultSet.close();
+                } catch (SQLException e) {
+                    e.printStackTrace();
+                }
+            }
+            if (connection != null) {
+                try {
+                    resultSet.close();
+                } catch (SQLException e) {
+                    e.printStackTrace();
+                }
+            }
+
+        }
+
+
+    }
+}
