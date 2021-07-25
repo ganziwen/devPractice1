4c3dcf87a795ecc74ef90630dbf9ee014bf18534
ganziwen <2422156774@qq.com>
2021-07-25 16:28:28 +0800

    🎉 增加设计模式模块

---
+++ patterns/src/main/java/App.java
@@ -0,0 +1,12 @@
+/**
+ * @author Ganziwen
+ * @version 1.0
+ * @ClassName App
+ * @Description
+ * @date 2021/7/25 16:27
+ */
+public class App {
+    public static void main(String[] args) {
+        System.out.println("hello");
+    }
+}
