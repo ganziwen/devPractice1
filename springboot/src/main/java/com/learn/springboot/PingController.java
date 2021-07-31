dcc4eb2a190a09e36e82a6886f2c9c7f2bd77426
ganziwen <2422156774@qq.com>
2021-08-01 02:29:12 +0800

    🎨 springboot-demo

---
+++ springboot/src/main/java/com/learn/springboot/PingController.java
@@ -0,0 +1,26 @@
+package com.learn.springboot;
+
+import com.sun.xml.internal.ws.server.ServerRtException;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @author Ganziwen
+ * @version 1.0
+ * @ClassName PingController
+ * @Description
+ * @date 2021/8/1 2:22
+ */
+
+@RestController
+public class PingController {
+    private static final Logger LOGGER = LoggerFactory.getLogger(PingController.class);
+
+    @RequestMapping("/ping")
+    public String ping() {
+        LOGGER.info("ping start!");
+        return "ping";
+    }
+}
