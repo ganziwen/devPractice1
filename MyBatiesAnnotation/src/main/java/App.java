b11d55db3021b5960db0ef307d2e428617626d71
steven01.gan <steven01.gan@vipshop.com>
2021-08-10 12:22:49 +0800

    🚧 修改 app 为 test

--- MyBatiesAnnotation/src/main/java/App.java
+++ MyBatiesAnnotation/src/main/java/App.java
@@ -4,6 +4,7 @@ import org.apache.ibatis.io.Resources;
 import org.apache.ibatis.session.SqlSession;
 import org.apache.ibatis.session.SqlSessionFactory;
 import org.apache.ibatis.session.SqlSessionFactoryBuilder;
+import org.junit.jupiter.api.Test;
 
 import java.io.IOException;
 import java.io.InputStream;
@@ -15,12 +16,8 @@ import java.util.List;
  * @date 2021/8/9-10:19
  */
 public class App {
-    public static void main(String[] args) {
-        // testSelectByUserId();
-        testInsertUser();
-    }
 
-    public static void testSelectByUserId() {
+    public SqlSession sqlSession() {
         // 创建一个配置文件流
         InputStream in = null;
         try {
@@ -34,6 +31,12 @@ public class App {
 
         // SqlSession 相当于连接池,并且开启自动提交
         SqlSession sqlSession = sqlSessionFactory.openSession(true);
+        return sqlSession;
+    }
+
+    @Test
+    public void testSelectByUserId() {
+        SqlSession sqlSession = sqlSession();
         TbUserMapper1 tbUser1Mapper = sqlSession.getMapper(TbUserMapper1.class);
         List<TbUser1> tu1s = tbUser1Mapper.selectByUserId("1000001");
         System.out.println("tu1s = " + tu1s);
@@ -42,20 +45,9 @@ public class App {
         sqlSession.close();
     }
 
-    public static void testSelectByUser() {
-        // 创建一个配置文件流
-        InputStream in = null;
-        try {
-            in = Resources.getResourceAsStream("mybaties-config.xml");
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-
-        //利用配置文件流利用工厂生成一个 SqlSessionFactory
-        SqlSessionFactory sqlSessionFactory = new SqlSessionFactoryBuilder().build(in, "development");
-
-        // SqlSession 相当于连接池,并且开启自动提交
-        SqlSession sqlSession = sqlSessionFactory.openSession(true);
+    @Test
+    public void testSelectByUser() {
+        SqlSession sqlSession = sqlSession();
         TbUserMapper1 tbUser1Mapper = sqlSession.getMapper(TbUserMapper1.class);
         List<TbUser1> tu1s = tbUser1Mapper.selectByUser1("1000002", "zhangsan2");
         System.out.println("tu1s = " + tu1s);
@@ -64,21 +56,9 @@ public class App {
         sqlSession.close();
     }
 
-
-    public static void testInsertUser() {
-        // 创建一个配置文件流
-        InputStream in = null;
-        try {
-            in = Resources.getResourceAsStream("mybaties-config.xml");
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-
-        //利用配置文件流利用工厂生成一个 SqlSessionFactory
-        SqlSessionFactory sqlSessionFactory = new SqlSessionFactoryBuilder().build(in, "development");
-
-        // SqlSession 相当于连接池,并且开启自动提交
-        SqlSession sqlSession = sqlSessionFactory.openSession(true);
+    @Test
+    public void testInsertUser() {
+        SqlSession sqlSession = sqlSession();
         TbUserMapper1 tbUser1Mapper = sqlSession.getMapper(TbUserMapper1.class);
         Integer insertNums = tbUser1Mapper.insertUser1(new TbUser1("1000004", "zhangsan4"));
         System.out.println("insertNums = " + insertNums);
