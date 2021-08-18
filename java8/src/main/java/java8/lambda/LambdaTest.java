42ec3249a0f12d3a59eb92ca3eea0a4c0afb7718
steven01.gan <steven01.gan@vipshop.com>
2021-08-18 10:31:33 +0800

    🎨 修改父项目 pom 文件,子模块可以引用到

---
+++ java8/src/main/java/java8/lambda/LambdaTest.java
@@ -0,0 +1,17 @@
+package java8.lambda;
+
+import org.junit.Test;
+
+/**
+ * @author steven01.gan
+ * @version 1.0
+ * @date 2021/8/18-10:22
+ */
+public class LambdaTest {
+
+    @Test
+    public void test1() {
+        System.out.println("LambdaTest.test1");
+    }
+
+}
