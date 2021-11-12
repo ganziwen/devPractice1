a0cf01e97b1ad6de7cbcf8c5394fa095d4409be4
steven01.gan <steven01.gan@vipshop.com>
2021-11-12 18:02:22 +0800

    🎨 了解一下注解怎么玩

--- Basic/src/main/java/annotation/test/TestTool.java
+++ Basic/src/main/java/annotation/test/TestTool.java
@@ -1,4 +1,5 @@
 package annotation.test;
+
 import annotation.NoBug;
 import annotation.annoer.JianCha;
 
@@ -18,34 +19,26 @@ public class TestTool {
         //用来记录测试产生的 log 信息
         StringBuilder log = new StringBuilder();
         // 记录异常的次数
-        int errornum = 0;
-        for ( Method m: method ) {
+        int errorNum = 0;
+        // 记录 pass 的次数
+        int successNum = 0;
+        for (Method m : method) {
             // 只有被 @Jiecha 标注过的方法才进行测试
-            if ( m.isAnnotationPresent( JianCha.class )) {
+            if (m.isAnnotationPresent(JianCha.class)) {
                 try {
+                    // 设置属性为可访问的
                     m.setAccessible(true);
                     m.invoke(testobj, null);
+                    successNum++;
                 } catch (Exception e) {
                     // TODO Auto-generated catch block
-                    //e.printStackTrace();
-                    errornum++;
-                    log.append(m.getName());
-                    log.append(" ");
-                    log.append("has error:");
-                    log.append("\n\r  caused by ");
-                    //记录测试过程中，发生的异常的名称
-                    log.append(e.getCause().getClass().getSimpleName());
-                    log.append("\n\r");
-                    //记录测试过程中，发生的异常的具体信息
-                    log.append(e.getCause().getMessage());
-                    log.append("\n\r");
+                    errorNum++;
+                    log.append(String.format("%s has error:\n\rcaused by:%s\n\r%s\n", m.getName(), e.getCause().getClass().getSimpleName(), e.getCause().getMessage()));
                 }
             }
         }
-        log.append(clazz.getSimpleName());
-        log.append(" has  ");
-        log.append(errornum);
-        log.append(" error.");
+        log.append(String.format("Report Info:\n %s\n pass %s cases.\nfailed %s cases", clazz.getSimpleName(), successNum, errorNum));
+
         // 生成测试报告
         System.out.println(log.toString());
     }
