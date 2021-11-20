ae174f37001e0d7db51aa551b55e4aa1f5c954ef
ganziwen <2422156774@qq.com>
2021-11-20 23:13:59 +0800

    ⚡ 暂时完成了 tag 和 group 的组合

--- auto-framework/src/main/java/com/example/autoframework/extention/engine/CaseDiscoveryFilter.java
+++ auto-framework/src/main/java/com/example/autoframework/extention/engine/CaseDiscoveryFilter.java
@@ -4,6 +4,7 @@ import com.example.autoframework.annotation.CaseGroup;
 import com.example.autoframework.annotation.CaseSelector;
 import com.example.autoframework.annotation.CaseTag;
 import org.junit.jupiter.engine.descriptor.TestMethodTestDescriptor;
+import org.junit.platform.commons.util.StringUtils;
 import org.junit.platform.engine.FilterResult;
 import org.junit.platform.engine.TestDescriptor;
 import org.junit.platform.launcher.PostDiscoveryFilter;
@@ -37,53 +38,45 @@ public class CaseDiscoveryFilter implements PostDiscoveryFilter {
         if (testDescriptor instanceof TestMethodTestDescriptor) {
             TestMethodTestDescriptor descriptor = (TestMethodTestDescriptor) testDescriptor;
             Method testMethod = descriptor.getTestMethod();
-            CaseTag[] caseTags = testMethod.getAnnotationsByType(CaseTag.class);
-            CaseGroup[] caseGroups = testMethod.getAnnotationsByType(CaseGroup.class);
-            // if (caseGroups.length > 0) {
-            //     CaseGroup onegroup = caseGroups[0];
-            // } else {
-            //     CaseGroup onegroup = null;
-            // }
-            long selectTagAndGroup = Arrays.stream(caseTags).filter(caseTag ->
-                    caseTag.key().equals(caseSelector.key()) && caseTag.val().equals(caseSelector.val())
-            ).count();
 
-            // tag 是 0-1 个
+            // 单个用例上的 tag ，可能会有多个，命中的可能也是多个(但是注解上只有一个，所以这里可以先绕过)
+            CaseTag[] caseTags = testMethod.getAnnotationsByType(CaseTag.class);
+            // 单个用例上的 group ，数量是 0-1 个
+            CaseGroup[] caseGroups = testMethod.getAnnotationsByType(CaseGroup.class);
+
+
+            // 命中的 tag 是 0-1 个
             long selectTagCount = Arrays.stream(caseTags).filter(caseTag ->
                     caseTag.key().equals(caseSelector.key()) && caseTag.val().equals(caseSelector.val())
             ).count();
 
-            // team 和 group 是 0-1 个
+            // 命中的 team 和 group 是 0-1 个
             long caseGroupsCount = Arrays.stream(caseGroups).filter(caseGroup ->
                     caseGroup.team().equals(caseSelector.team()) && caseGroup.group().equals(caseSelector.group())
             ).count();
 
-            switch (caseTags.length + caseGroups.length) {
-                case 0:
+            // 根据标签是否空白区反选匹配到的 count 数量，这部分比较冗余看看怎么优化一下
+            if (StringUtils.isNotBlank(caseSelector.key()) && StringUtils.isNotBlank(caseSelector.team())) {
+                if (selectTagCount == 1 && caseGroupsCount == 1) {
+                    return FilterResult.includedIf(true);
+                } else {
                     return FilterResult.includedIf(false);
-                case 1:
-                    if (caseTags.length == selectTagCount && caseGroups.length == caseGroupsCount) {
-                        return FilterResult.includedIf(true);
-                    }
-                case 2:
-                    if (caseTags.length == selectTagCount && caseGroups.length == caseGroupsCount) {
-                        return FilterResult.includedIf(true);
-                    }
-                default:
-                    break;
+                }
+            } else if (StringUtils.isBlank(caseSelector.key()) && StringUtils.isNotBlank(caseSelector.team())) {
+                if (caseGroupsCount == 1) {
+                    return FilterResult.includedIf(true);
+                } else {
+                    return FilterResult.includedIf(false);
+                }
+            } else if (StringUtils.isNotBlank(caseSelector.key()) && StringUtils.isBlank(caseSelector.team())) {
+                if (selectTagCount == 1) {
+                    return FilterResult.includedIf(true);
+                } else {
+                    return FilterResult.includedIf(false);
+                }
+            } else {
+                return FilterResult.includedIf(false);
             }
-
-            // // 没有 key 以及 team
-            // if ((caseTags.length + caseGroups.length) == 0) {
-            //     return FilterResult.includedIf(false);
-            //
-            // } else if ((caseTags.length + caseGroups.length) == 0) {
-            // }
-            //
-            // if (selectTagCount > 0 || caseGroupsCount > 0) {
-            //     // 命中 tag 规则之后，就 include 进去
-            //     return FilterResult.includedIf(true);
-            // }
         }
         return FilterResult.includedIf(false);
     }
