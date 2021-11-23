3e5e20000ced01f91e4caeb0f785edf080f61c3c
steven01.gan <steven01.gan@vipshop.com>
2021-11-23 21:14:37 +0800

    加注释

--- auto-framework/src/main/java/com/example/autoframework/cases/accout/TestAccount.java
+++ auto-framework/src/main/java/com/example/autoframework/cases/accout/TestAccount.java
@@ -3,6 +3,8 @@ package com.example.autoframework.cases.accout;
 import com.example.autoframework.annotation.*;
 import org.junit.jupiter.api.DisplayName;
 
+import static com.example.autoframework.enums.Project.LEVEL_NORMAL;
+
 /**
  * @author Ganziwen
  * @version 1.0
@@ -21,24 +23,24 @@ public class TestAccount {
     @CheckPoint("检查点2")// 为 case 检查点，必填
     @CaseTag(key = "project", val = "头条")// 为 case 添加标签后方便后续运行时做筛选，必填
     @CaseTag(key = "module", val = "Order")// 为 case 添加标签后方便后续运行时做筛选，必填
-    @CaseTag(key = "level", val = "redline")// 为 case 添加标签后方便后续运行时做筛选，必填
+    @CaseTag(key = "level", val = LEVEL_NORMAL)// 为 case 添加标签后方便后续运行时做筛选，必填
     @CaseGroup(team = "test_team", group = "test_group")
     public void testNormal1() {
         System.out.println("TestAccount.testNormal3");
     }
 
-    // @AutoTest
-    // @DisplayName("测试支付-正常-3")
-    // @CaseTitle("正常用例-3") // 给 case 加标题便于后续查找和区分，必填
-    // @CaseDesc(desc = "测试支付", owner = "Ganziwen")
-    // @DingTalkAlarm(token = "testToken") // 用于报警处理
-    // @CheckPoint("检查点1")// 为 case 检查点，必填
-    // @CheckPoint("检查点2")// 为 case 检查点，必填
-    // @CaseTag(key = "project", val = "头条")// 为 case 添加标签后方便后续运行时做筛选，必填
-    // @CaseTag(key = "module", val = "Order")// 为 case 添加标签后方便后续运行时做筛选，必填
-    // @CaseTag(key = "level", val = "redline")// 为 case 添加标签后方便后续运行时做筛选，必填
-    // @CaseGroup(team = "test_team1", group = "test_group")
-    // public void testNormal2() {
-    //     System.out.println("TestAccount.testNormal3");
-    // }
+    @AutoTest
+    @DisplayName("测试支付-正常-3")
+    @CaseTitle("正常用例-3") // 给 case 加标题便于后续查找和区分，必填
+    @CaseDesc(desc = "测试支付", owner = "Ganziwen")
+    @DingTalkAlarm(token = "testToken") // 用于报警处理
+    @CheckPoint("检查点1")// 为 case 检查点，必填
+    @CheckPoint("检查点2")// 为 case 检查点，必填
+    @CaseTag(key = "project", val = "头条")// 为 case 添加标签后方便后续运行时做筛选，必填
+    @CaseTag(key = "module", val = "Order")// 为 case 添加标签后方便后续运行时做筛选，必填
+    @CaseTag(key = "level", val = "redline")// 为 case 添加标签后方便后续运行时做筛选，必填
+    @CaseGroup(team = "test_team1", group = "test_group")
+    public void testNormal2() {
+        System.out.println("TestAccount.testNormal3");
+    }
 }
---
+++ auto-framework/src/main/java/com/example/autoframework/enums/Project.java
@@ -0,0 +1,12 @@
+package com.example.autoframework.enums;
+
+/**
+ * @author steven01.gan
+ * @version 1.0
+ * @date 2021/11/23-21:01
+ */
+public class Project {
+    public static final String LEVEL_NORMAL = "NORMAL";
+    public static final String LEVEL_RED_LINE = "redline";
+
+}
